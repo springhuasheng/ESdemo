865b6c54abafa1948cb41241e8408150af4a9043
luckspring <10105307+luckspring@user.noreply.gitee.com>
2022-01-11 18:45:27 +0800

    no message

--- src/main/java/cn/itcast/hotel/pojo/Hotel.java
+++ src/main/java/cn/itcast/hotel/pojo/Hotel.java
@@ -9,8 +9,10 @@ import lombok.Data;
 @Data
 @TableName("tb_hotel")
 public class Hotel {
+
     @TableId(type = IdType.INPUT)
     private Long id;
+
     private String name;
     private String address;
     private Integer price;
@@ -19,13 +21,17 @@ public class Hotel {
     private String city;
     private String starName;
     private String business;
+
+    //转化JSON 将此字段排除
     @JsonIgnore
     private String longitude;
     @JsonIgnore
     private String latitude;
+
     private String pic;
     private String location;
 
+    //获取Location时将 longitude + ", " + latitude 拼接返回
     public String getLocation() {
         return this.longitude + ", " + this.latitude;
     }
